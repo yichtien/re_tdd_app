2be45283c72c29fe3757d5a1bb63c6190bf90a90
jiaguicheng <jiaguicheng@linezonedata.com>
2020-02-03 16:01:15 +0800

    started

---
+++ flask-tdd-docker/project/__init__.py
@@ -0,0 +1,17 @@
+# project/__init__.py
+from flask import Flask, jsonify
+from flask_restplus import Resource, Api
+
+app = Flask(__name__)
+
+api = Api(app)
+
+app.config.from_object('project.config.DevelopmentConfig')
+
+
+class Ping(Resource):
+    def get(self):
+        return jsonify({'status': 'success', 'message': 'pong!'})
+
+
+api.add_resource(Ping, '/ping')
