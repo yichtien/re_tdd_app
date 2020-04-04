d339b9a046d27add60274e40f80ab9061cba1878
huajin <tien4yich@gmail.com>
2020-04-04 20:51:27 +0800

    [add]: postgresql setup.

---
+++ flask-tdd-docker/services/users/project/config.py
@@ -0,0 +1,26 @@
+# services/users/project/config.py
+
+
+import os  # new
+
+
+class BaseConfig:
+    """Base configuration"""
+    TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False  # new
+
+
+class DevelopmentConfig(BaseConfig):
+    """Development configuration"""
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
+
+
+class TestingConfig(BaseConfig):
+    """Testing configuration"""
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')  # new
+
+
+class ProductionConfig(BaseConfig):
+    """Production configuration"""
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
